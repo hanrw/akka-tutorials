5484ccdc5dea9b35068e72e79f42ad3c16276bc9
slamhan <slamhan1987@hotmail.com>
2020-05-10 15:54:27 +0800

    initial for seckill

--- seckill/src/main/scala/org/hanrw/akka/seckill/SecKillApplication.scala
+++ seckill/src/main/scala/org/hanrw/akka/seckill/SecKillApplication.scala
@@ -18,13 +18,14 @@ object SecKillApplication {
     val system = ActorSystem("SecKill", ConfigFactory.load())
     system.actorOf(Props(new SkuActorFactory(skuId, amountOfSku)), "skus")
     Thread.sleep(1000)
-    println(s"开始抢购 ${System.currentTimeMillis()}")
-    0 until  users foreach { userId =>
+    val date = System.currentTimeMillis()
+    println(s"开始抢购 $date")
+    0 until users foreach { userId =>
       val idx = userId % amountOfSku
       val skuActor = system.actorSelection(s"/user/skus/sku:actor:$skuId:$idx")
       val secKillRequest = SecKillRequest(userId, skuId)
       skuActor ! secKillRequest
     }
-    println("Done!")
+    println(s"在${System.currentTimeMillis() - date}毫秒秒内抢购结束 ")
   }
 }
